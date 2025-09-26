1db956ab6ca4c017570075f458a6a8dc130c32af
CescDev <medicalsos068@gmail.com>
2025-09-26 15:50:02 -0600

    Agregando API de buscar equipo

--- src/components/team/AddMemberForm.tsx
+++ src/components/team/AddMemberForm.tsx
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom";
 import { useMutation } from "@tanstack/react-query";
 import ErrorMessage from "../ErrorMessage";
 import type { TeamMemberForm } from "@/types/index";
+import { findUserByEmail } from "@/api/TeamAPI";
+import Loaders from "../Loaders";
+import ImageEmpty from "../imageUI/ImageEmpty";
 
 export default function AddMemberForm() {
   const initialValues: TeamMemberForm = {
@@ -18,9 +21,17 @@ export default function AddMemberForm() {
     formState: { errors },
   } = useForm({ defaultValues: initialValues });
 
-  const mutation = useMutation({});
+  const mutation = useMutation({
+    mutationFn: findUserByEmail
+  });
 
-  const handleSearchUser = async () => {};
+  const handleSearchUser = async (formData: TeamMemberForm) => {
+    const data = { projectId, formData }
+    mutation.mutate(data);
+    console.log(data)
+
+    console.log(mutation);
+  };
 
   return (
     <>
@@ -55,6 +66,16 @@ export default function AddMemberForm() {
           value="Buscar Usuario"
         />
       </form>
+
+      <div className="flex items-center justify-center mt-6">
+        {mutation.isPending && (<Loaders />)}
+        {
+          mutation.isError &&
+          (
+            <p className="text-center text-red-500 uppercase">{mutation.error.message}</p>
+          )
+        }
+      </div>
     </>
   );
 }
